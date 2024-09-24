a04c2214ef987c174ae30a39e9a34f0862514e0c
RusFortunat <ruslan.mukhamadiarov@gmail.com>
2024-09-24 21:58:38 +0200

    started working on Network class

--- src/main/java/Network.java
+++ src/main/java/Network.java
@@ -3,5 +3,29 @@
  */
 
 public class Network {
+    private int inputSize;
+    private int hiddenSize;
+    private int outputSize;
+    private double[][] firstLayerWeights;
+    private double[] firstLayerBiases;
+    private double[][] secondLayerWeights;
+    private double[] secondLayerBiases;
+    
+    // at the moment just 1 hidden layer, generalization will come later
+    public Network(int inputSize, int hiddenSize, int outputSize){
+        this.inputSize = inputSize;
+        this.hiddenSize = hiddenSize;
+        this.outputSize = outputSize;
+        this.firstLayerWeights = new double[inputSize][hiddenSize];
+        this.firstLayerBiases = new double[hiddenSize];
+        this.secondLayerWeights = new double[hiddenSize][outputSize];
+        this.secondLayerBiases = new double[outputSize];
+    }
+    
+    public double[] forward(double[] input){
+        double[] predicted = new double[outputSize];
+        
+        return predicted;
+    }
     
 }
