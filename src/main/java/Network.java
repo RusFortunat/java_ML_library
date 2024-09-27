ad353a88c543b11c0de28f6fe727afa0316fa4c4
RusFortunat <ruslan.mukhamadiarov@gmail.com>
2024-09-27 17:41:26 +0200

    finished Network.java

--- src/main/java/Network.java
+++ src/main/java/Network.java
@@ -1,10 +1,11 @@
 /**
- * @author RusFortunat
+ * @author RusFortunat, i.e., Ruslan Mukhamadiarov
  */
 import java.util.concurrent.ThreadLocalRandom;
 
 // at the moment just 1 hidden layer, generalization will come later
 public class Network {
+    private double learningRate;
     private int inputSize;
     private int hiddenSize;
     private int outputSize;
@@ -12,9 +13,12 @@ public class Network {
     private double[] firstLayerBiases;
     private double[][] secondLayerWeights;
     private double[] secondLayerBiases;
+    private double[] hiddenVector;
+    private double[] outputVector;
     
     // initialize a fully-connected neural network with random weights and biases
-    public Network(int inputSize, int hiddenSize, int outputSize){
+    public Network(double learningRate, int inputSize, int hiddenSize, int outputSize){
+        this.learningRate = learningRate;
         this.inputSize = inputSize;
         this.hiddenSize = hiddenSize;
         this.outputSize = outputSize;
@@ -22,8 +26,10 @@ public class Network {
         this.firstLayerBiases = new double[hiddenSize];
         this.secondLayerWeights = new double[hiddenSize][outputSize];
         this.secondLayerBiases = new double[outputSize];
+        this.hiddenVector = new double[hiddenSize];
+        this.outputVector = new double[outputSize];
         
-        double rangeW1 = 1.0/inputSize; // it is a good practice to distribte 
+        double rangeW1 = 1.0/inputSize; // it is a good practice to limit distribution to inverse vector size 
         for(int j = 0; j < hiddenSize; j++){
             firstLayerBiases[j] = ThreadLocalRandom.current().nextDouble(-rangeW1,rangeW1);
             for(int i = 0; i < hiddenSize; i++) {
@@ -39,11 +45,88 @@ public class Network {
         }
     }
     
-    // implements feed-forward propagation with reLU activation
-    public double[] forward(double[] input){
-        double[] predicted = new double[outputSize];
-        
-        return predicted;
+    // implements feed-forward propagation with ReLU activation
+    public void forward(double[] input){
+        // compute hidden activation values
+        for(int i = 0; i < hiddenSize; i++){
+            double sum = 0;
+            for(int j = 0; j < inputSize; j++){
+                double activation = firstLayerWeights[i][j]*input[j] + firstLayerBiases[i];
+                if(activation > 0) sum+= activation; // ReLU activation
+            }
+            hiddenVector[i] = sum;
+        }
+        // compute output activations
+        for(int i = 0; i < outputSize; i++){
+            double sum = 0;
+            for(int j = 0; j < hiddenSize; j++){
+                double activation = secondLayerWeights[i][j]*input[j] + secondLayerBiases[i];
+                if(activation > 0) sum+= activation; // ReLU activation
+            }
+            outputVector[i] = sum;
+        }
+
+        // SoftMax
+        double sum = 0;
+        for(Double element:outputVector) sum += Math.exp(element);
+        for(Double element:outputVector) element = Math.exp(element) / sum;
     }
     
+    // Implements error backpropagation: computes the error and updates the NN parameters accordingly;
+    // Math behind the backpropagation in NNs is nicely explained in 3b1b youtube video on this topic
+    public void backpropagateError(double[] inputVector, double[] targetVector, int batchSize){
+        double[][] GRADfirstLayerWeights = new double[inputSize][hiddenSize];
+        double[] GRADfirstLayerBiases = new double[hiddenSize];
+        double[][] GRADsecondLayerWeights = new double[hiddenSize][outputSize];
+        double[] GRADsecondLayerBiases = new double[outputSize];
+
+        // Second(final) layer W2 & B2 gradients
+        for(int i=0; i < outputSize; i++){
+            if(outputVector[i] > 0){ // comes from ReLU's derivative
+                for(int j = 0; j < hiddenSize; j++){
+                    if(hiddenVector[j] != 0){
+                        GRADsecondLayerWeights[i][j] += (1.0/batchSize) * 2 * (outputVector[i] - targetVector[i]) * hiddenVector[j];
+                    }
+                }
+                GRADsecondLayerBiases[i] += (1.0/batchSize) * 2 * (outputVector[i] - targetVector[i]);
+            }
+        }
+        // First(starting) layer W1 & B1 gradients
+        for(int i = 0; i < hiddenSize; i++){
+            if(hiddenVector[i] > 0){ // comes from ReLU's derivative
+                double secondLayerGrad = 0;
+                for(int k = 0; k < outputSize; k++){
+                    if(outputVector[k] > 0){ // comes from ReLU's derivative
+                        secondLayerGrad += 2.0 * (outputVector[k] - targetVector[k]) * secondLayerWeights[k][i];
+                    }
+                }
+                for(int j = 0; j < inputSize; j++){
+                    if(inputVector[j] != 0){
+                        GRADfirstLayerWeights[i][j] += (1.0/batchSize) * secondLayerGrad * inputVector[j];
+                    }
+                }
+                GRADfirstLayerBiases[i] += (1.0/batchSize) * secondLayerGrad;
+            }
+        }
+
+        // update neural network parameters
+        // first layer W1 & B1
+        for(int i = 0; i < hiddenSize; i++){
+            for(int j = 0; j < inputSize; j++){
+                firstLayerWeights[i][j] -= learningRate * GRADfirstLayerWeights[i][j];
+            }
+            firstLayerBiases[i] -= learningRate * GRADfirstLayerBiases[i];
+        }
+        // second layer W2 & W2
+        for(int i = 0; i < outputSize; i++){
+            for(int j = 0; j < hiddenSize; j++){
+                secondLayerWeights[i][j] -= learningRate * GRADsecondLayerWeights[i][j];
+            }
+            secondLayerBiases[i] -= learningRate * GRADsecondLayerBiases[i];
+        }
+    }
+
+    public Object[] networkParameteres(){
+        return new Object[]{firstLayerWeights, firstLayerBiases, secondLayerWeights, secondLayerBiases};
+    }
 }
