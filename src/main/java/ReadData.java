433e03c6ff9775b424abcd1ad29410066c3293e5
RusFortunat <ruslan.mukhamadiarov@gmail.com>
2024-09-27 17:56:45 +0200

    created ReadData class

---
+++ src/main/java/ReadData.java
@@ -0,0 +1,28 @@
+/**
+ * @author RusFortunat, i.e., Ruslan Mukhamadiarov
+ */
+import java.util.ArrayList;
+
+// at the moment just 1 hidden layer, generalization will come later
+public class ReadData {
+    private String fullPath;
+    private Object itames;
+    private ArrayList<String> labels;
+
+    public ReadData(String path){
+        this.fullPath = path;
+    }
+
+    public void loadImages(){
+
+    }
+
+    public void loadLabels(){
+
+        
+    }
+
+
+
+
+}
